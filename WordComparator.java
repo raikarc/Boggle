d6cb800df76dc6a4f0875fe1dafbf469445a8358
raikarc <137921766+raikarc@users.noreply.github.com>
2024-06-07 23:36:23 -0700

    Add files via upload

---
+++ WordComparator.java
@@ -0,0 +1,28 @@
+
+// Jessica Qian, APCS Peterson 2
+// Programming Assignment Chapter 12
+
+import java.util.Comparator;
+
+public class WordComparator implements Comparator<String> {
+	public WordComparator() {
+	}
+
+	// overrides compare method to sort based on length and alphabetize it
+	@Override
+	public int compare(String arg0, String arg1) {
+		if (arg0.length() > arg1.length()) {
+			return -1;
+		} else if (arg0.length() < arg1.length()) {
+			return 1;
+		}
+		for (int i = 0; i < arg0.length(); i++) {
+			if (arg0.charAt(i) > arg1.charAt(i)) {
+				return 1;
+			} else if (arg0.charAt(i) < arg1.charAt(i)) {
+				return -1;
+			}
+		}
+		return 0;
+	}
+}
